0331f9a2aeda692bafe70084b479aaae9b0c8734
Quido Veekens <QuidoVeekens@hotmail.com>
2021-05-18 14:29:26 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+  /*
+
+
+  //menu 
+    const menuButton = document.getElementById('burgerMenu');
+    menuButton.addEventListener('mousedown', stay);
+
+    let Clicked = 0;
+
+    function stay() {
+        
+        Clicked++;
+    
+        if (Clicked%2==0) {
+           return collapse()
+        } else {
+            return expand()
+        }
+    }
+
+
+    function expand(){
+        
+        const full = document.getElementById('fullMenu');
+        full.style.display = 'block';
+        
+    }
+
+    function collapse(){
+        
+        const full = document.getElementById('fullMenu');
+        full.style.display = 'none';
+        
+    }
+*/
+   
